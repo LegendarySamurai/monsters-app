c833bc8ec31a321da9627f95fef339521e84347a
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-01-04 11:13:04 +0200

    Updates

---
+++ precache-manifest.6b952c361cd2b8f2c81d20864b9fd119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23176dc679d875b462459b0e80498cd3",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "8fdf8170f982d1891831",
+    "url": "/monsters-app/static/css/main.d4abe0f0.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-app/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-app/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8fdf8170f982d1891831",
+    "url": "/monsters-app/static/js/main.bf987727.chunk.js"
+  },
+  {
+    "revision": "a205996d0c5533b8ce48",
+    "url": "/monsters-app/static/js/runtime-main.64344b14.js"
+  }
+]);
